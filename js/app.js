cec40de0ebf1ec7632d61724b579fc23ef4bbd8e
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-04-24 18:30:17 -0400

    cleaning code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
-/* YOUTUBE FUNCTIONALITY */
+/* STATE OBJECT */
+var stateObject = {
+	// What info do you need to store?
+	// (language, type, and problem) submitted by user
+	// RACHEL NOTE: Clear out everything on the state when
+	// the user hits submit -- before it's populated
+	// by API calls
+};
 
-// FUNCTIONAL => Returning Proper Data
+
+/* STATE MOD FUNCTIONS: Everything you can do to state */
+function storeSearch (stateObject, userSearch) {
+	// push 3 search terms (language, type, and problem) to state
+}
+
+
+/* YOUTUBE */
+
+// EDIT TO RETURN VIDEO INFO NEEDED FOR PLAYER
 var YOUTUBE_API = "https://www.googleapis.com/youtube/v3/search";
 var youTubeCallParams = {
 	part: 'snippet',
@@ -10,12 +26,10 @@ var youTubeCallParams = {
 
 // SUCCESS CALLBACK => FUNCTIONAL => To Be Replaced w/ DOM Mod Function
 function sendYouTubeToDOM (data) {
+
 	console.log("Success!!! You got " + data.items.length + " YouTube results back.");
 	data.items.forEach(function (item) {
 		console.log(item);
-
-		// DOM MOD PLACEHOLDER
-		$('.rp-video-display').append(jawn);
 	});
 }
 
@@ -34,43 +48,39 @@ var jawn =  "<video width='320' height='240' controls>" +
   				"Your browser does not support the video tag" +
 			"</video>";
 
+/* MEETUP */
 
+// NOT FUNCTIONAL => Ran into issues w/ Acces-Control-Allow-Origin Error
+// Will be implemented when app is rebuild w/ Node & Express
 
-/* MEETUP FUNCTIONALITY */
-
-// NOT FUNCTIONAL => Need to figure out how to use OAUTH
-
-// var city = "";
-// var state = "";
-// var topic = "";
-// var MEETUP_API = "https://api.meetup.com/2/open_events?sign=true&photo-host=public&country=US&page=5&key=3b2c776267d212831792c3364645c1&dataType=jsonp" + 
-// 	"&city=" + city + "&state=" + state + "&topic=" + topic + "";
-// var MEETUP_API = "";
-
-// var meetupCallParams = {
-// 	dataType: 'jsonp',
-// 	method: 'get',
-// 	key: '3b2c776267d212831792c3364645c1'
-// };
-
+var topic = "";
+var MEETUP_API = "https://api.meetup.com/2/open_events?sign=true&photo-host=public&country=US&topic=" + topic + "t&city=Washington&state=DC&page=20&key=68617c6b1d76f47132910394a4b3a9";
+var meetupCallParams = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.meetup.com/2/open_events?sign=true&photo-host=public&country=US&topic=JavaScript&city=Washington&state=DC&page=20&key=68617c6b1d76f47132910394a4b3a9",
+  "method": "GET",
+  "type": "jsonp",
+  "key": "68617c6b1d76f47132910394a4b3a9",
+  "headers": {
+    "authorization": "Basic cnlhbmJzaGVycmlsbDpJbGxzdGVhbHlvdTEzIQ==",
+    "cache-control": "no-cache",
+    "postman-token": "3004bc85-9e53-1c49-c0c9-6ada36a258a4"
+  }
+};
 
 // SUCCESS CALLBACK => FUNCTIONAL => To Be Replaced w/ DOM Mod Function
 
-// function sendMeetupToDOM (data) {
-// 	console.log(" Success!!! You got " + data.meta.count + " Meetup results back.");
-// 	data.results.forEach(function (result) {
-// 		console.log(result);
-// 		console.log(result.name);
-
-// 		// DOM MOD PLACEHOLDER
-// 		$('.rp-meetup-section').append("<p>" + result.name + "</p>");
-// 		$('.rp-meetup-section').append("<p>" + result.event_url + "</p>");
-// 	});
-// }
+function sendMeetupToDOM (data) {
+	console.log(" Success!!! You got " + data.meta.count + " Meetup results back.");
+	data.results.forEach(function (result) {
+		console.log(result);
+		console.log(result.name);
+	});
+}
 
 
-
-/* QUOTE FUNCTIONALITY */
+/* QUOTES */
 
 // FUNCTIONAL => Random Pull From Quote Repo Is Working
 var CODING_QUOTES = [
@@ -91,52 +101,37 @@ var CODING_QUOTES = [
 	},
 ];
 
-// SUCCESS TEST => To Be Replaced w/ DOM Mod Function
+// SUCCESS CALLBACK => To Be Replaced w/ DOM Mod Function
 function getQuote (CODING_QUOTES) {
 	return CODING_QUOTES[ Math.floor(Math.random() * CODING_QUOTES.length) ];
 }
 
 
+/* DOM MOD FUNCTIONS */
 
-/* STATE OBJECT */
-var stateObject = {
-	// What info do you need to store?
-	// (language, type, and problem) submitted by user
-	// RACHEL NOTE: Clear out everything on the state when
-	// the user hits submit -- before it's populated
-	// by API calls
-};
-
-
-
-/* STATE MOD FUNCTIONS: Everything you can do to state */
-function storeSearch (stateObject, userSearch) {
-	// push 3 search terms (language, type, and problem) to state
-}
-
-/* DOM MOD FUNCTIONS: Functions that render state */
-// Two arguments: state object, and a DOM element to render into
 function hideLandingPage () {
-	// target landing page and hide it
 	$('#lp').toggleClass('hidden');
 }
+
 function unhideResultsPage () {
 	$('#rp').toggleClass('hidden');
 }
+
+
 function sendQuoteToDOM (quote) {
 	var quoteToSend = getQuote(CODING_QUOTES);
 	console.log(quoteToSend.name);
 	console.log(quoteToSend.quote);
 	console.log(quoteToSend.source);
-
-
-	$('.rp-quote-section').html(
-		
-		"<h1 class='centered'>" + quoteToSend.quote + "</h1>" +
-		"<h3>" + "- " + quoteToSend.name + "</h3>"
-	);
 }
 
+
+// 	$('.rp-quote-section').html(
+		
+// 		"<h1 class='centered'>" + quoteToSend.quote + "</h1>" +
+// 		"<h3>" + "- " + quoteToSend.name + "</h3>"
+// 	);
+// }
 
 
 
@@ -145,85 +140,46 @@ var getYouTubeData = function() {
 	$.getJSON(YOUTUBE_API, youTubeCallParams, displayResults);
 };
 
-var getMeetupData = function() {
-	$.getJSON(MEETUP_API, meetupCallParams, sendMeetupToDOM);
-};
+// var getMeetupData = function() {
+// 	$.getJSON(MEETUP_API, meetupCallParams, sendMeetupToDOM);
+// };
 
-var getMdnData = function() {
-	$.getJSON(MDN_API, mdnCallParams, successCallback);
-};
+// var getMdnData = function() {
+// 	$.getJSON(MDN_API, mdnCallParams, successCallback);
+// };
 
-/* EVENT LISTENERS */
 
-// NEED TO MAKE => Listener for valid entry by user
+/* LISTENERS */
+
+
+
 $('.lp-search-submit').on('click', function (e) {
 	e.preventDefault();
-	
-	// GET USER INPUT FOR VIDEOS
+
+	// NEED TO MAKE => Listener for valid entry by user ***
+
+	// get user input => youtube
 	youTubeCallParams.q = $('.lp-search-input').val();
 	
+	// get user input => meetup
+	//topic += $('.lp-search-input').val().replace(/[^a-zA-Z0-9 ]/g, "").split(' ').join('%');
 
-	// GET USER INPUT FOR MEETUP API CALL => MODIFY MEETUP_API value
+	// test user input collected correctly
+	//console.log(MEETUP_API);
 
-	// city += $('.lp-city').val().replace(/[^a-zA-Z0-9 ]/g, "").split(' ').join('%');
-	// state += $('.lp-state').val().replace(/[^a-zA-Z0-9 ]/g, "").split(' ').join('%');
-	// topic += $('.lp-search-input').val().replace(/[^a-zA-Z0-9 ]/g, "").split(' ').join('%');
-
-	//var MEETUP_API = "https://api.meetup.com/2/open_events?sign=true&photo-host=public&country=US&city=Washington&state=DC&page=5&key=3b2c776267d212831792c3364645c1&dataType=jsonp&topic=" + topic + "";
-	// console.log(MEETUP_API);
-
-	// MAKE REQUESTS
+	// api requests
 	getYouTubeData();
-	// getMeetupData();
+	//getMeetupData();
 
-
-
-	// MODIFY THE DOM
+	// modify dom elements 
 	hideLandingPage();
 	unhideResultsPage();
-	sendQuoteToDOM();
+	// MAKE THIS FUNCTION
+	// sendQuoteToDOM();
+	
+	
 
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function () {
-
-
-// 		// RUN API CALLS:
-
-// 		// YouTube
-// 		console.log(getYouTubeData());
-// 	}
-
-// 		// meetup
-// 		// MDN
-
-// 		// store user search in state
-
-// 		// run DOM functions
-// 		// sendQuoteToDOM
-// 		// hideLandingPage
-// 		// unhideResultsPage
-// });
-		
-
-// // $(/* target #results-page SUBMIT BUTTON */).submit(function () {
-		
-// // 		// store user search in state
-
-// // 		// run DOM functions
-// // 		// sendQuoteToDOM
-// // 	});
-// // });
